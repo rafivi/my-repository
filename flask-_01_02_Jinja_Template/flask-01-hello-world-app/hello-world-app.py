63787492241272298a265e0207cec0ce618b2a2b
rafivi <rafivi7@gmail.com>
2024-08-12 20:12:40 -0400

    add for if files

---
+++ flask-_01_02_Jinja_Template/flask-01-hello-world-app/hello-world-app.py
@@ -0,0 +1,29 @@
+from flask import Flask 
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello World"
+
+@app.route("/second")
+def second():
+    return"This is the second page"
+
+@app.route("/third/subthird")
+def third():
+    return "This is the subpage of the third page"
+
+@app.route("/whichpage/<string:page_id>")
+def whichpage(page_id): 
+    if page_id.isdigit():
+        message = f"The id of this page is {page_id}" 
+    else:
+        message = f"{page_id} is not a valid id"
+
+    return message
+
+
+if __name__ == '__main__':
+
+    app.run(debug=True, port=30000)
+    # app.run(host= '0.0.0.0', port=8081)
